93e92d384ee2a2b09a49cdff44ea85a15a96bc6a
SaraP <sara_pargaricohan@ultimatesoftware.com>
2021-01-09 18:41:58 -0500

    sending real estate calculator link to twilio

--- src/main/java/com/twilio/app/App.java
+++ src/main/java/com/twilio/app/App.java
@@ -2,12 +2,19 @@ package com.twilio.app;
 
 import static spark.Spark.*;
 
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.twiml.voice.Say;
 import com.twilio.twiml.VoiceResponse;
 import com.twilio.exception.ApiException;
 import com.twilio.type.PhoneNumber;
+import com.twilio.base.ResourceSet;
+import com.twilio.rest.api.v2010.account.Call;
 
 
 public class App {
@@ -15,30 +22,61 @@ public class App {
         port(8080);
         post("/answer", (req, res) -> {
             // get the urlencoded form parameters
-            String caller = req.queryParams("From");
-            String twilioNumber = req.queryParams("To");
+			Map<String, String> bodyMap = bodyToMap(req.body());
+			String decodedCallerNumber  = URLDecoder.decode(bodyMap.get("UserIdentifier"), "UTF-8");
 
-            sendSms(caller, twilioNumber);
+            sendSms(decodedCallerNumber);
             VoiceResponse twiml = new VoiceResponse.Builder()
                 .say(new Say.Builder("Thanks for calling! We just sent you a text with a clue.")
                       .voice(Say.Voice.ALICE)
                       .build())
                 .build();
 
-            return twiml.toXml();
+			// Render TwiML as XML
+			res.type("text/xml");
+
+			String xml = twiml.toXml();
+
+			JSONArray array = new JSONArray();
+			JSONObject sayJson = new JSONObject().put("say", "Thanks for calling!  We just sent you a "
+					+ "text with a "
+					+ "clue.");
+			JSONObject redirectJson = new JSONObject().put("redirect", "task://goodbye");
+			array.put(sayJson)
+					.put(redirectJson);
+			String jsonString = new JSONObject()
+					.put("actions", array)
+					.toString();
+
+
+			return jsonString;
         });
+
     }
 
-    public static void sendSms(String toNumber, String fromNumber) {
-        String accountSid = System.getenv("ACCOUNT_SID");
+    public static void sendSms(String toNumber) {
+		String fromNumber = null;
+		String accountSid = System.getenv("ACCOUNT_SID");
         String authToken = System.getenv("AUTH_TOKEN");
+		Twilio.init(accountSid, authToken);
 
-        try {
-            Twilio.init(accountSid, authToken);
+		ResourceSet<Call> calls = Call.reader().limit(1).read();
+		for(Call record : calls) {
+			//System.out.println(record.getSid());
+			fromNumber = record.getTo();
+		}
+
+		try {
             Message
                 .creator(new PhoneNumber(toNumber),
                          new PhoneNumber(fromNumber),
-                        "There's always money in the banana stand.")
+                        "Please use our Closing Costs Calculator here: http://closings.lexpand.ca. "
+								+ "Select your transaction type and enter the required information. "
+								+ "Enter your email address at the last step and the system will automatically email "
+								+ "a fairly detailed funds summary to you. If you wish to retain us for your closing, "
+								+ "click on the Hire Us Now button in the email and submit the form. "
+								+ "We will then open your file and send you the initial correspondence with more information "
+								+ "about the rest of the process.")
                 .create();
         } catch (ApiException e) {
             if (e.getCode() == 21614) {
@@ -46,4 +84,16 @@ public class App {
             }
         }
     }
+
+	public static Map<String, String> bodyToMap(String bodyStr) {
+		Map<String, String> body = new HashMap<>();
+		String[] values = bodyStr.split("&");
+		for (String value : values) {
+			String[] pair = value.split("=");
+			if (pair.length == 2) {
+				body.put(pair[0], pair[1]);
+			}
+		}
+		return body;
+	}
 }
